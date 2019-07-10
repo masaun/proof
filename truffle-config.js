8fcc535f07764e9cc4c6e7a75a9f008420b2f0ff
masaun <masanoriuno@gmail.com>
2019-07-10 19:27:21 +0200

    Add dotenv to truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,22 @@
+const HDWalletProvider = require("truffle-hdwallet-provider");
+
+const mnemonic = process.env.MNEMONIC;
+const infura_api_key = process.env.INFURA_API_KEY;
+
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  networks: {
+    development: {
+      host: "127.0.0.1",
+      port: 7545,     // Ganache-GUI
+      //port: 8545,   // Ganache-CLI
+      network_id: "*",
+    },
+    ropsten: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, "https://ropsten.infura.io/v3/" + infura_api_key);
+      },
+      network_id: '3',
+    }
+  }
 };
