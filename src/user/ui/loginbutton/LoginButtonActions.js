60808f7e674d6ac27037df65704bd58940c73585
masaun <masanoriuno@gmail.com>
2019-07-13 09:21:01 +0200

    Migrate Profile.sol and Read Profile.sol in LoginButtonActions.js

--- src/user/ui/loginbutton/LoginButtonActions.js
+++ src/user/ui/loginbutton/LoginButtonActions.js
@@ -1,5 +1,7 @@
 import { uport } from './../../../util/connectors.js'
 import { browserHistory } from 'react-router'
+import getWeb3, { getGanacheWeb3, Web3 } from '../../../util/getWeb3'
+
 
 export const USER_LOGGED_IN = 'USER_LOGGED_IN'
 function userLoggedIn(user) {
@@ -18,6 +20,33 @@ export function loginUser() {
 
       // [Debug]
       console.log('=== credentials（created by uport.requestCredentials()）===', credentials)
+
+      // Read contract of Profile.sol
+      let Profile = {};
+      try {
+        Profile = require("../../../../build/contracts/Profile.json");
+      } catch (e) {
+        console.log(e);
+      }
+      const web3 = new Web3(window.ethereum);
+      const accounts = web3.eth.getAccounts();
+      const networkId = web3.eth.net.getId();
+      const ContractAddress = Profile['networks'][networkId]['address'];
+      
+      let instanceProfile = null;
+      instanceProfile = new web3.eth.Contract(Profile.abi, ContractAddress);
+      console.log('=== Profile["networks"]["5777"]["address"] ===', Profile['networks']['5777']['address']);
+      console.log('=== instanceProfile ===', instanceProfile);
+
+      if (instanceProfile) {
+        // Set web3, accounts, and contract to the state, and then proceed with an
+        // example of interacting with the contract's methods.
+        this.setState({ web3, accounts, instanceProfile: instanceProfile });
+      }
+        
+
+      // Save in blockchain
+
 
       // Used a manual redirect here as opposed to a wrapper.
       // This way, once logged in a user can still access the home page.
