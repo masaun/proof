5e2fbd5ed2e690c893b503b25f931a7042610ef7
masaun <masanoriuno@gmail.com>
2019-07-13 10:02:18 +0200

    Get contract address of Profile.sol in LoginButtonActions.js

--- src/user/ui/loginbutton/LoginButtonActions.js
+++ src/user/ui/loginbutton/LoginButtonActions.js
@@ -32,28 +32,66 @@ export function loginUser() {
         console.log(e);
       }
       const web3 = new Web3(window.ethereum);
-      const accounts = web3.eth.getAccounts();
-      const networkId = web3.eth.net.getId();
-      const ContractAddress = Profile['networks'][networkId]['address'];
+
+      //const accounts = web3.eth.getAccounts();
+      //const networkId = web3.eth.net.getId();
+      const accounts = [];
+      const networkId = [];
+      web3.eth.getAccounts().then((accounts) => {
+        console.log('=== accounts ===', accounts);   // Success
+        console.log('=== accounts[0] ===', accounts[0]);   // Success
+
+        web3.eth.net.getId().then((networkId) => {
+          console.log('=== networkId ===', networkId); // Success
+
+          const ContractAddress = Profile['networks'][networkId]['address'];
+          
+          let instanceProfile = null;
+          instanceProfile = new web3.eth.Contract(Profile.abi, ContractAddress);
+          console.log('=== Profile["networks"]["5777"]["address"] ===', Profile['networks']['5777']['address']);
+          console.log('=== instanceProfile ===', instanceProfile);
+
+          if (instanceProfile) {
+            // Set web3, accounts, and contract to the state, and then proceed with an
+            // example of interacting with the contract's methods.
+            this.setState({ web3, accounts, instanceProfile: instanceProfile });
+          }
+
+          // Save in blockchain
+          instanceProfile.methods.saveUser(credentials['address'], credentials['did'], credentials['name']).send({ from: this.state.accounts[0] })
+
+          // Get saved value in struct
+          instanceProfile.methods.getUser(accounts[0]).call().then((s) => {
+            console.log('== s ==', s);
+          })
+        })
+      })      
+
+      // web3.eth.net.getId().then((networkId) => {
+      //   console.log('=== networkId ===', networkId); // Success
+      // })
       
-      let instanceProfile = null;
-      instanceProfile = new web3.eth.Contract(Profile.abi, ContractAddress);
-      console.log('=== Profile["networks"]["5777"]["address"] ===', Profile['networks']['5777']['address']);
-      console.log('=== instanceProfile ===', instanceProfile);
+      // const ContractAddress = Profile['networks'][networkId]['address'];
+      
+      // let instanceProfile = null;
+      // instanceProfile = new web3.eth.Contract(Profile.abi, ContractAddress);
+      // console.log('=== Profile["networks"]["5777"]["address"] ===', Profile['networks']['5777']['address']);
+      // console.log('=== instanceProfile ===', instanceProfile);
 
-      if (instanceProfile) {
-        // Set web3, accounts, and contract to the state, and then proceed with an
-        // example of interacting with the contract's methods.
-        this.setState({ web3, accounts, instanceProfile: instanceProfile });
-      }
+      // if (instanceProfile) {
+      //   // Set web3, accounts, and contract to the state, and then proceed with an
+      //   // example of interacting with the contract's methods.
+      //   this.setState({ web3, accounts, instanceProfile: instanceProfile });
+      // }
 
-      // Save in blockchain
-      instanceProfile.methods.saveUser(credentials['address'], credentials['did'], credentials['name']).send({ from: this.state.accounts[0] })
+      // // Save in blockchain
+      // instanceProfile.methods.saveUser(credentials['address'], credentials['did'], credentials['name']).send({ from: this.state.accounts[0] })
 
-      // Get saved value in struct
-      instanceProfile.methods.getUser(accounts[0]).call().then((s) => {
-        console.log('== s ==', s);
-      })
+      // // Get saved value in struct
+      // instanceProfile.methods.getUser(accounts[0]).call().then((s) => {
+      //   console.log('== s ==', s);
+      // })
+
 
       // Used a manual redirect here as opposed to a wrapper.
       // This way, once logged in a user can still access the home page.
