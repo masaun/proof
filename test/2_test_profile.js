c2fc3c4e92377b9ad29d148c39ce0e6772e23b3b
masaun <masanoriuno@gmail.com>
2019-07-14 09:47:25 +0200

    Sucess to execute getUser function of Profile.sol on Tuffle test

--- test/2_test_profile.js
+++ test/2_test_profile.js
@@ -2,7 +2,7 @@ var Profile = artifacts.require("./Profile.sol");
 
 contract('=== Profile ===', function(accounts) {
 
-  it("...should store the user data from uPort.", function() {
+  it("...should save user data from uPort.", function() {
     return Profile.deployed().then(function(instance) {
       profileInstance = instance;
 
@@ -12,16 +12,18 @@ contract('=== Profile ===', function(accounts) {
     });
   });
 
-  // it("...should store the user data from uPort.", function() {
-  //   return Profile.deployed().then(function(instance) {
-  //     profileInstance = instance;
 
-  //     return profileInstance.saveUser("0x6464835fdb341a46bffe7a25d63f6d9076e3032a", "did:ethr:0x.....", "Taro Yamada", {from: accounts[0]});
-  //   }).then(function() {
-  //     return profileInstance.getUser("0x6464835fdb341a46bffe7a25d63f6d9076e3032a").call();
-  //   }).then(function(getSavedUserData) {
-  //     console.log('=== get savedUserData ===', getSavedUserData)
-  //   });
-  // });
+
+  it("...should get saved user data from uPort.", function() {
+    return Profile.deployed().then(function(instance) {
+      profileInstance = instance;
+
+      return profileInstance.saveUser("0x6464835fdb341a46bffe7a25d63f6d9076e3032a", "did:ethr:0x.....", "Taro Yamada", {from: accounts[0]});
+    }).then(function() {
+      return profileInstance.getUser("0x6464835fdb341a46bffe7a25d63f6d9076e3032a");
+    }).then(function(getSavedUserData) {
+      console.log('=== get savedUserData ===', getSavedUserData)
+    });
+  });
 
 });
